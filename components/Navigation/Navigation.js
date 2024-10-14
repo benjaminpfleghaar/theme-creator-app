746a042b2223a2f72b22fe30b4cbabae88becce8
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-10-14 09:10:36 +0200

    add logo

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
 import Tab from "@/components/Tab/Tab";
 import { useThemeState } from "@/libs/states";
 import "@/components/Navigation/Navigation.scss";
@@ -10,10 +12,10 @@ export default function Navigation() {
 	return (
 		<header className="navigation">
 			<nav className="navigation__nav">
+				<Link href="/" title="Home" className="navigation__logo">
+					<Image src="/images/logo.svg" width={40} height={40} alt="Theme Creator" />
+				</Link>
 				<ul className="navigation__nav-list">
-					<li>
-						<Tab title="Home" href="/" />
-					</li>
 					{themes.map((theme) => (
 						<li key={theme.id}>
 							<Tab title={theme.name} href={`/${theme.id}`} />
